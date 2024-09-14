1ab4714726961409e9625ed8862fab1a4ac87c22
Hassan Siddiqui <103386392+hassan7865@users.noreply.github.com>
2024-09-14 18:16:04 +0500

    Update Url.js

--- src/Url.js
+++ src/Url.js
@@ -1,6 +1,6 @@
 // import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Req = axios.create({
-  baseURL: `https://fine-erin-giraffe-tie.cyclic.app/api`,
+  baseURL: `https://nac-backend.vercel.app/api`,
 });
 export default Req;
